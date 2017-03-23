80a02506c0b0e7380099d7fe9b44ec4a1bd636ab
Kalnik Maksim <kalnikmaxim@gmail.com>
2017-03-23 16:31:58 +0200

    gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
-'use strict';
- 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+var watch = require('gulp-watch');
+
+gulp.task("sass", function(){
+	gulp.src("sass/style.scss")
+		.pipe(sass())
+		.pipe(gulp.dest('css'))
 });
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
+gulp.task('watch', function(){
+  gulp.watch('sass/style.scss', ['sass']); 
+})
