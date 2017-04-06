7acb64578cd0292a40ce79acd32dcc0dbcf3efb5
Kalnik Maksim <kalnikmaxim@gmail.com>
2017-04-06 18:47:53 +0300

    ad

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
+var gulp      = require('gulp'), // Подключаем Gulp
+    sass        = require('gulp-sass'), //Подключаем Sass пакет,
+    browserSync = require('browser-sync'); // Подключаем Browser Sync
 
-gulp.task("sass", function(){
-	gulp.src("sass/style.scss")
-		.pipe(sass())
-		.pipe(gulp.dest('css'))
+gulp.task('sass', function(){ // Создаем таск Sass
+    return gulp.src('sass/style.scss') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
-gulp.task('watch', function(){
-  gulp.watch('sass/style.scss', ['sass']); 
-})
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: '' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+    gulp.watch('sass/**/*.{scss, sass}', ['sass']); // Наблюдение за sass файлами
+    // Наблюдение за другими типами файлов
+});
